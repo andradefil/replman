3d01020253426d302e1f256bcbcd64c1af66636b
Filipe Andrade <support@prolambdas.com>
2026-06-07 17:46:37 -0300

    Add custom REPL with support to special forms

---
+++ README.md
@@ -0,0 +1,9 @@
+# REPLMAN
+<img src="docs/logo.svg" alt="REPLMAN Logo" width="200"/>
+
+Tooling for managing my REPL workflow.
+
+Launch a REPL
+```
+(#'replman.repl/repl)
+```
---
+++ src/replman/repl.clj
@@ -0,0 +1,50 @@
+;; Based on code from Nubank Morse
+;; Copyright (c) Nu North America, Inc. All rights reserved.
+;; Modifications Copyright Filipe Andrade
+
+(ns replman.repl
+  (:require [clojure.core.async :as async]
+            [clojure.main :as main]
+            [clojure.core.server :as server]
+            [dev.nu.morse :as morse]))
+
+(def ^:private echan (var-get #'dev.nu.morse/echan))
+
+(defn eval-special-forms!
+  [val]
+  (cond
+    (= val :repl/morse-ui)
+    (morse/ui :proc server/prepl :mode :in-proc)))
+
+(defn repl []
+  (apply require main/repl-requires)
+  (println "Clojure" (clojure-version))
+  (printf "%s=> " (ns-name *ns*))
+  (flush)
+  (let [ch (async/chan)
+        o *out*
+        ex? (every-pred :cause :via)
+        cb (fn [{:keys [tag val form ms ns] :as m}]
+             (when-not (= tag :tap)
+               (async/offer! echan (assoc m :rebl/source "REPL")))
+             (eval-special-forms! val)
+             (binding [*out* o]
+               (case tag
+                 :err (do (print val) (flush))
+                 :out (do (print val) (flush))
+                 :tap nil
+                 :ret
+                 (do
+                   (if (:exception m)
+                     (binding [*out* *err*]
+                       (print (-> val main/ex-triage main/ex-str))
+                       (flush))
+                     (prn val))
+                   (printf "%s=> " ns)
+                   (flush)))))]
+    (server/prepl *in* cb)))
+
+(comment
+  (in-ns 'replman.repl)
+  (#'dev.nu.morse/launch-in-proc)
+  (#'replman.repl/repl))
